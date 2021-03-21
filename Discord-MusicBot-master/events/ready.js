94abd2657bdad7a91c9db27b5c6c8276071c3849
hasiburrupom <68180307+hasiburrupom@users.noreply.github.com>
2021-03-21 17:15:27 +0600

    Update ready.js

--- Discord-MusicBot-master/events/ready.js
+++ Discord-MusicBot-master/events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity(" -help  -By RUPOmღ", {
+  await client.user.setActivity(" -help | By RUPOmღ", {
     type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
